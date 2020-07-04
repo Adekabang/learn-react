abbcff4ab8ca3ffb15f5bf088c29bbd7c7c8e678
Mohammad Raska <iban.ninja@gmail.com>
2020-07-05 01:03:26 +0700

    change App.js component to class/container component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Ninjas from './Ninjas'
-function App() {
-const ninjas = [
-  { name: "Ryu", age: 30, belt: "black", id: 1 },
-  { name: "Yoshi", age: 20, belt: "green", id: 2 },
-  { name: "Benedict Cumberbatch", age: 43, belt: "Old black", id: 3 }
-]
 
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <p>Welcome To my Web😉</p>
-      <Ninjas ninjas={ninjas}/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Benedict Cumberbatch", age: 43, belt: "Old black", id: 3 }
+    ]
+  } 
+  render() {
+    return (
+      <div className="App">
+        <h1>My First React App</h1>
+        <p>Welcome To my Web😉</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
